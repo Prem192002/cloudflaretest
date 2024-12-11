64f106734acb4342da8374b5cdfbd7fba818c14a
Prem singh <98180839+Prem192002@users.noreply.github.com>
2024-12-12 00:27:26 +0530

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Explore templates for Next.js.
           </p>
         </a>
 
